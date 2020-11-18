09eff80b7fbd6f696ee883bdc6ded169b38bc4d5
Guerryc972 <72447874+Guerryc972@users.noreply.github.com>
2020-11-18 08:38:01 +0100

    Add files via upload

---
+++ exercice1711/exercice2/js/main.js
@@ -0,0 +1,12 @@
+function changer_style() {
+  var p = document.getElementById("paragraphe");
+  p.style.color = "white";
+  p.style.backgroundColor = "#bbb";
+  p.style.border = "1px solid gray";
+  p.style.padding = "5px";
+}
+
+function changer_style() {
+  var p = document.getElementById("paragraphe");
+  p.classList.add("active");
+}
